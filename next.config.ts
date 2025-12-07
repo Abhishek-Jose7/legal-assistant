9b09505a8e7a818391a7ae7f45dea89c86d82ac7
Abhishek-Jose7 <abhishekjose780@gmail.com>
2025-12-07 10:10:49 +0530

    Remove webpack config to fix Turbopack build error

--- next.config.ts
+++ next.config.ts
@@ -16,11 +16,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
-  },
 };
 
 export default nextConfig;
